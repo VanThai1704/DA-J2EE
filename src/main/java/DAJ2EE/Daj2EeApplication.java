feec449e95ca4d488fde3562e9016edbef36d1d9
VanThai1704 <thainro129@gmail.com>
2026-03-23 09:59:45 +0700

    springboot

---
+++ src/main/java/DAJ2EE/Daj2EeApplication.java
@@ -0,0 +1,13 @@
+package DAJ2EE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Daj2EeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Daj2EeApplication.class, args);
+	}
+
+}
